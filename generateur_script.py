54dd8daf99776e3c65dbcaf73173eb4dc3c3dd7c
L-L-E-N-A-L <lena.fougeron@etu.univ-amu.fr>
2026-03-23 16:29:15 +0100

    ajout script pour grade

--- generateur_script.py
+++ generateur_script.py
@@ -8,13 +8,12 @@ fake = Faker(locale="fr_CA")
 liste_prenom = ([unidecode(fake.unique.first_name_male()).upper() for _ in range(70)],[unidecode(fake.unique.first_name_female()).upper() for _ in range(70)]) # 0 = homme , 1 = femme
 liste_nom = [unidecode(fake.unique.last_name()).upper() for _ in range(250)]
 
-liste_grade = (["AFFILIE", "SYMPATISANT", "ADHERANT", "CHEVALIER", "GRAND CHEVALIER", "COMMANDEUR" , "GRAND CROIX"],["AFFILIE", "SYMPATISANT", "ADHERANT", "DAME", "HAUTE DAME", "COMMANDEUR" , "GRAND-CROIX"])
+liste_grade = (["AFFILIE", "SYMPATISANT", "ADHERANT", "CHEVALIER", "GRAND CHEVALIER", "COMMANDEUR" , "GRAND CROIX"],["AFFILIEE", "SYMPATISANTE", "ADHERANTE", "DAME", "HAUTE DAME", "COMMANDERESSE" , "GRANDE-CROIX"])
 liste_rang = ["'NOVICE'","'COMPAGNON'"]
 liste_titre = ["'PHILANTROPHE'","'PROTECTEUR'","'HONORABLE'"]
 liste_dignite = ["'MAITRE'","'GRAND CHANCELIER'","'GRAND MAITRE'"]
 
-codes_villes = pa.read_csv("./codes_villes")
-codes_villes = codes_villes[["Nom_de_la_commune","Code_postal"]]
+codes_villes = pa.read_csv("./codes_villes.csv", encoding="latin-1")
 
 def email_generator(nom,prenom):
     email = ""
@@ -76,6 +75,15 @@ for _ in range(100_000):
     if i==1 : 
         data_tenrac["sexe"] = 'F'
         data_tenrac["prenom"] = liste_prenom[1][randint(0,len(liste_prenom[0])-1)]
+        data_tenrac["grade"] = liste_grade[1][randint(0,len(liste_grade)-1)]
 
     file.write(f"INSERT INTO Tenrac(idTenrac,nomT,prenomT,courriel,tel,adresseT,sexe,typeRang,typeTitre,typeDignite,typeGrade) VALUES({data_tenrac["id"]},'{data_tenrac["nom"]}','{data_tenrac["prenom"]}','{email_generator(data_tenrac["nom"],data_tenrac["prenom"])}','{data_tenrac["tel"]}','{data_tenrac["adresse"]}','{data_tenrac["sexe"]}',{data_tenrac["rang"]},{data_tenrac["titre"]},{data_tenrac["dignite"]},'{data_tenrac["grade"]}'); \n")
-    
+
+# GRADE
+for i in range(len(liste_grade[0])-1) :
+
+    if i == len(liste_grade[0]-1) :
+
+        file.write(f"INSERT INTO Tenrac(typeGrade) VALUES ({liste_grade[0][i]})")
+    else :
+        file.write(f"INSERT INTO Tenrac(typeGrade, superieurGrade) VALUES ({liste_grade[0][i]},{liste_grade[0][i+1]})")
